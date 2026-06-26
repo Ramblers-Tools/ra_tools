2c57860046400b670294677782ffc6ac005f643d
Richard Higham <richard.higham@hahosting.com>
2026-06-26 08:58:39 +0100

    Install database schema from admin component

--- build-package.sh
+++ build-package.sh
@@ -9,7 +9,7 @@ package="${component}-${version}.zip"
 rm -rf dist
 mkdir -p dist
 
-zip -r "dist/${package}" "$manifest" admin forms languages sql src tmpl \
+zip -r "dist/${package}" "$manifest" admin forms languages src tmpl \
 	-x '.DS_Store' \
 	-x '*/.DS_Store' \
 	-x '*.old' \
